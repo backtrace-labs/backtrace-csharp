4559a1878b23c15b5b8357097783e66a1de9be1e
Konrad Dysput <konrad.dysput@hotmail.com>
2018-02-05 22:52:32 +0100

    Updated ExceptionStack class API description

--- Backtrace/Model/ExceptionStack.cs
+++ Backtrace/Model/ExceptionStack.cs
@@ -4,13 +4,39 @@ using System.Text;
 
 namespace Backtrace.Model
 {
+    /// <summary>
+    /// Parse exception information to Backtrace API format
+    /// </summary>
     internal class ExceptionStack
     {
+        /// <summary>
+        /// Function where exception occurs
+        /// </summary>
         public string FunctionName { get; set; }
+
+        /// <summary>
+        /// Line number in source code where exception occurs
+        /// </summary>
         public int Line { get; set; }
+
+        /// <summary>
+        /// Column number in source code where exception occurs
+        /// </summary>
         public int Column { get; set; }
+
+        /// <summary>
+        /// Source code file name where exception occurs
+        /// </summary>
         public string SourceCode { get; set; }
+
+        /// <summary>
+        /// Library name where exception occurs
+        /// </summary>
         public string Library { get; set; }
+
+        /// <summary>
+        /// Check if there is an inner exception
+        /// </summary>
         public bool CallstackState { get; set; }
 
         /// <summary>
@@ -23,6 +49,7 @@ namespace Backtrace.Model
             {
                 return;
             }
+            //get a current stack frame from an exception
             var stackTrace = new System.Diagnostics.StackTrace(exception, true);
             var frame = stackTrace?.GetFrame(stackTrace.FrameCount - 1);
             if (frame == null)
