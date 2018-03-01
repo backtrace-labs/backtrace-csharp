e4d39ab0585baa7d9680eec788bb4dffc3778da1
Konrad Dysput <konrad.dysput@hotmail.com>
2018-03-02 00:29:39 +0100

    Complex sample app

--- Backtrace.Core/Program.cs
+++ Backtrace.Core/Program.cs
@@ -1,41 +1,159 @@
-﻿using Backtrace.Interfaces;
+﻿using Backtrace.Core.Model.DataStructures.Trees;
 using Backtrace.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading;
 
-namespace Backtrace.Core
+namespace Backtrace.Core.Model
 {
     class Program
     {
-        private static BacktraceClient backtraceClient = new BacktraceClient();
-        private static void DoSomething(int i = 0)
-        {
-            if (i == 2)
-            {
-                throw new ArgumentException("i");
-            }
-            Thread.Sleep(20);
-            try
-            {
-                DoSomething(++i);
+        private bool treeGenerated = false;
+        private Tree tree;
+        private HashSet<Thread> threads = new HashSet<Thread>();
+        //initialize new BacktraceClient with custom configuration section readed from file App.config
+        //Client will be initialized with values stored in default section name "BacktraceCredentials"
+        private BacktraceClient backtraceClient = new BacktraceClient(
+            reportPerMin: 0 //unlimited number of reports per secound
+        );
 
-            }
-            catch (Exception e)
+        public Program()
+        {
+            SetupBacktraceLibrary();
+            SetupStartupTasks();
+            StartTasks();
+        }
+
+        private void StartTasks()
+        {
+            foreach (var task in threads)
             {
-                backtraceClient.Send(e);
+                task.Start();
+            }
+            foreach (var task in threads)
+            {
+                task.Join();
             }
         }
-        static void Main(string[] args)
-        {
-            //initialize new BacktraceClient with custom configuration section readed from file App.config
-            //Client will be initialized with values stored in default section name "BacktraceCredentials"
-            //var backtraceClient = new BacktraceClient();
 
-            var credentials = new BacktraceCredentials("https://yourHostUrl.com", "accessToken");
-            var backtraceClientWithCredentials = new BacktraceClient(credentials);
+        private string GetRandomString(int length)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var tempString = Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray();
+            return new string(tempString);
+        }
+
+        private void GenerateRandomStrings()
+        {
+            Random random = new Random();
+            int totalStrings = random.Next(20, 25);
+            //+3 because we want to add two duplicats
+            string[] randomStrings = new string[totalStrings + 3];
+            for (int i = 0; i < totalStrings; i++)
+            {
+                int stringLength = random.Next(0, 8);
+                randomStrings[i] = GetRandomString(stringLength);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                int copyIndex = random.Next(0, totalStrings);
+                randomStrings[totalStrings + i] = randomStrings[copyIndex];
+            }
+            Thread.Sleep(100);
+            tree = new Tree(backtraceClient);
+            for (int i = 0; i < totalStrings + 3; i++)
+            {
+                try
+                {
+                    tree.Add(randomStrings[i]);
+                }
+                catch (Exception exception)
+                {
+                    var result = backtraceClient.Send(exception);
+                    //we catch exception inside three add method
+                    continue;
+                }
+            }
+            Thread.Sleep(150);
+            EndTreeGeneration();
+        }
+
+        private void TryClean()
+        {
+            if (!treeGenerated)
+            {
+                Thread.Sleep(100);
+                TryClean();
+                return;
+            }
+
+            var orderedWords = tree.ToList();
+            int total = orderedWords.Count();
+            Random random = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                int randomDuplicateIndex = random.Next(0, total);
+                orderedWords.Add(orderedWords.ElementAt(randomDuplicateIndex));
+            }
+            orderedWords.Add(GetRandomString(5));
+            orderedWords.Add(string.Empty);
+            var shuffledWords = orderedWords.OrderBy(x => random.Next()).ToArray();
+            RemoveWords(shuffledWords);
+        }
+
+        private void RemoveWords(string[] shuffledWords)
+        {
+            for (int i = 0; i < shuffledWords.Length; i++)
+            {
+                try
+                {
+                    string word = shuffledWords[i];
+                    tree.Remove(word);
+                }
+                catch
+                {
+                    //we catch exception inside three add method
+                    continue;
+                }
+            }
+            EndCleaning();
+
+        }
+
+        private void EndCleaning()
+        {
+            backtraceClient.Send($"{DateTime.Now}: Tree clean");
+        }
+
+        private void EndTreeGeneration()
+        {
+            treeGenerated = true;
+            backtraceClient.Send($"{DateTime.Now}: Tree generated");
+        }
+
+        /// <summary>
+        /// Prepare multithreading calculations
+        /// </summary>
+        private void SetupStartupTasks()
+        {
+            threads.Add(new Thread(new ThreadStart(() => { TryClean(); })));
+            threads.Add(new Thread(new ThreadStart(() => { GenerateRandomStrings(); })));
+
+        }
+
+        /// <summary>
+        /// Setup client behaviour - attributes, events
+        /// </summary>
+        private void SetupBacktraceLibrary()
+        {
 
             //Add new scoped attributes
+
             backtraceClient.Attributes["ClientAttributeNumber"] = 1;
             backtraceClient.Attributes["ClientAttributeString"] = "string attribute";
             backtraceClient.Attributes["ClientAttributeCustomClass"] = new
@@ -43,35 +161,26 @@ namespace Backtrace.Core
                 Name = "Backtrace",
                 Type = "Library"
             };
+            backtraceClient.Attributes["ComplexObject"] = new Dictionary<string, Uri>()
+            {
+                {"backtrace.io" , new Uri("http://backtrace.io") },
+                {"Google url" , new Uri("http://google.com") }
+            };
 
             //Add your own handler to client API
             backtraceClient.BeforeSend =
-               (Model.BacktraceData<object> model) =>
+               (BacktraceData<object> model) =>
                {
                    var data = model;
+                   data.Attributes.Add("eventAtrtibute", "EventAttributeValue");
+                   return data;
                };
 
-            //Report a new exception from current application
-            try
-            {
-                Thread thread = new Thread(new ThreadStart(() => { DoSomething(0); }));
-                thread.Start();
-                thread.Join();
-                var i = 0;
-                var result = i / i;
-            }
-            catch (Exception exception)
-            {
-                var report = new BacktraceReport<object>(
-                    exception: exception,
-                    attributes: new Dictionary<string, object>() { { "AttributeString", "string" } },
-                    attachmentPaths: new List<string>() { @"path to file attachment", @"patch to another file attachment" }
-                );
-                backtraceClient.Send(report);
-            }
-
-            //Report a new message
-            backtraceClient.Send("Client message");
+            backtraceClient.Send($"{DateTime.Now}: Library Initialized");
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
         }
     }
 }
