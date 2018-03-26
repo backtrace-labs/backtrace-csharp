9a1df3a63aa91ca687ffa5cbde4c06907704231d
Konrad Dysput <konrad.dysput@hotmail.com>
2018-03-27 01:42:48 +0200

    Exception extensions

---
+++ Backtrace/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Backtrace.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backtrace.Extensions
+{
+    /// <summary>
+    /// Extensions method available for every excepton object
+    /// </summary>
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        /// Convert current exception to BacktraceReport instance
+        /// </summary>
+        /// <param name="source">Current exception</param>
+        /// <returns>Backtrace Report</returns>
+        public static BacktraceReport ToBacktraceReport(this Exception source)
+        {
+            return new BacktraceReport(source);
+        }
+    }
+}
