5c4e36db3b7ada4c629766cac56117660cca43d8
Konrad Dysput <konrad.dysput@gmail.com>
2018-05-05 12:09:14 +0200

    Backtrace UWP example with database

---
+++ Backtrace.UWP/Model/ApplicationCredentials.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backtrace.UWP.Model
+{
+    public static class ApplicationCredentials
+    {
+        public const string Host = @"https://yolo.sp.backtrace.io:6098/";
+        public const string Token = "328174ab5c377e2cdcb6c763ec2bbdf1f9aa5282c1f6bede693efe06a479db54";
+    }
+}
