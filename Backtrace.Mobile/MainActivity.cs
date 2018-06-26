5950e9626a5b1d4ff3fdc5c3e3dad2da0e0d5e1d
Konrad Dysput <konrad.dysput@gmail.com>
2018-06-26 22:15:21 +0200

    Xamarin sample project small code refactor

--- Backtrace.Mobile/MainActivity.cs
+++ Backtrace.Mobile/MainActivity.cs
@@ -14,7 +14,6 @@ namespace Backtrace.Mobile
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
             //setup database 
             // we get external storage directory and special directory created for databse - Backtrace directory
             string directoryPath = Path.Combine(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath, "Backtrace");
@@ -26,15 +25,11 @@ namespace Backtrace.Mobile
 
             // Initialize new BacktraceClient 
             BacktraceClient client = new BacktraceClient(clientConfiguration, database);
-            try
-            {
-                // Send async report to a server with custom client message
-                var result = client.SendAsync("Hello from Xamarin").Result;
-            }
-            catch(Exception e)
-            {
-                System.Diagnostics.Trace.WriteLine(e.Message);
-            }
+
+            // Send async report to a server with custom client message
+            var result = client.SendAsync("Hello from Xamarin").Result;
+
+            base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
         }
