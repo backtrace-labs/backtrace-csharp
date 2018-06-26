8dd342d0e4fe62ef0cfcf47f3eed93f63e3290f9
Konrad Dysput <konrad.dysput@gmail.com>
2018-06-26 22:30:55 +0200

    Preparing sample apps

--- Backtrace.Mobile/Model/ApplicationSettings.cs
+++ Backtrace.Mobile/Model/ApplicationSettings.cs
@@ -2,8 +2,8 @@
 {
     public static class ApplicationSettings
     {
-        public const string Host = @"http://yolo.sp.backtrace.io:6097/";
-        public const string Token = "328174ab5c377e2cdcb6c763ec2bbdf1f9aa5282c1f6bede693efe06a479db54";
+        public const string Host = @"https://myserver.sp.backtrace.io:6097";
+        public const string Token = "4dca18e8769d0f5d10db0d1b665e64b3d716f76bf182fbcdad5d1d8070c12db0";
 
         //Check MainActivity.cs class to check how we prepare BacktraceDatabase instance
         //For Xamarin solution we prepare special directory called Backtrace in the root of file system
